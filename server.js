8f306262d372156ab00ba121ae1acb1a4f396519
Jonathan Landrey Smith <jonathansmith@jonathans-mbp.uconnect.utah.edu>
2018-03-13 17:11:37 -0600

    file

--- server.js
+++ server.js
@@ -3,36 +3,44 @@ var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
 var axios = require("axios");
 var cheerio = require("cheerio");
 
+// Require all models
 var db = require("./models");
 
 var PORT = 3000;
 
+// Initialize Express
 var app = express();
 
+// Configure middleware
 
-
+// Use morgan logger for logging requests
 app.use(logger("dev"));
-
+// Use body-parser for handling form submissions
 app.use(bodyParser.urlencoded({ extended: false }));
-
+// Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoScraper";
+// By default mongoose uses callbacks for async queries, we're setting it to use promises (.then syntax) instead
+// Connect to the Mongo DB
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
-//app.get to scrape webpage
+//routes here
 
-//app.get for getting articles from db
+//
 
-//app.get for grabbing specific article by id
-
-//app.post for saving/updating an article's associated note
-
+// Start the server
 app.listen(PORT, function() {
     console.log("App running on port " + PORT + "!");
-})
+});
+  
