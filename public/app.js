645cd4ec489552e897443b108c920bac60db4a3b
Jonathan Landrey Smith <jonathansmith@jonathans-mbp.uconnect.utah.edu>
2018-03-15 23:32:59 -0600

    file

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 $.getJSON("/articles", function(data) {
 
     for (var i = 0; i < data.length; i++) {
-        $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].headline + "<br />" + data[i].summary + "<br />" + data[i].link + "</p>");
+        $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].headline + "<br />" + data[i].summary + "<br />Get more information <a href='" + data[i].link + "'>here<a>.</p>");
     }
 });
 
